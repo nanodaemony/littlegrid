dc159b80029c40267f9c327b5f222a7d0e032409
nano <1174520425@qq.com>
2026-04-06 00:15:40 +0800

    feat(grid-app): add TokenProvider, Controller and SecurityConfig

---
+++ backend/grid-app/src/main/java/com/naon/grid/modules/app/config/AppSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.naon.grid.modules.app.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@Order(101)
+@RequiredArgsConstructor
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .requestMatchers()
+            .antMatchers("/api/app/**")
+            .and()
+            .csrf().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/app/auth/register").anonymous()
+            .antMatchers("/api/app/auth/login").anonymous()
+            .anyRequest().authenticated();
+    }
+}
