088e593bf722d67ff7ae1b82806fa1d0456586e3
nano <1174520425@qq.com>
2026-04-12 16:56:53 +0800

    refactor: remove username query methods from GridUserRepository

--- backend/grid-app/src/main/java/com/naon/grid/modules/app/repository/GridUserRepository.java
+++ backend/grid-app/src/main/java/com/naon/grid/modules/app/repository/GridUserRepository.java
@@ -14,19 +14,9 @@ import java.util.Optional;
 public interface GridUserRepository extends JpaRepository<GridUser, Long>, JpaSpecificationExecutor<GridUser> {
 
     /**
-     * 根据用户名查询用户
-     */
-    Optional<GridUser> findByUsername(String username);
-
-    /**
      * 根据手机号查询用户
      */
     Optional<GridUser> findByPhone(String phone);
-
-    /**
-     * 根据用户名判断是否存在
-     */
-    boolean existsByUsername(String username);
 
     /**
      * 根据手机号判断是否存在
